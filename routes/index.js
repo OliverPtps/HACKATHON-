1346c53046e5da3a9e30a02ce142968e652f01b8
OliverPtps <oliver.printemps@yahoo.fr>
2022-04-14 11:48:05 +0200

    BDD connection

--- routes/index.js
+++ routes/index.js
@@ -10,18 +10,6 @@ var options = {
   useUnifiedTopology: true
  };
 
-// --------------------- BDD -----------------------------------------------------
-mongoose.connect('mongodb+srv://XXXXXXXX:*********@XXXXXXXX-0hsfc.mongodb.net/Ticketac?retryWrites=true',
-   options,
-   function(err) {
-    if (err) {
-      console.log(`error, failed to connect to the database because --> ${err}`);
-    } else {
-      console.info('*** Database Ticketac connection : Success ***');
-    }
-   }
-);
-
 var journeySchema = mongoose.Schema({
   departure: String,
   arrival: String,
@@ -36,42 +24,41 @@ var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lil
 var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
 
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Express' });
 });
 
 
-// Remplissage de la base de donnée, une fois suffit
-router.get('/save', async function(req, res, next) {
+// // Remplissage de la base de donnée, une fois suffit
+// router.get('/save', async function(req, res, next) {
 
-  // How many journeys we want
-  var count = 300
+//   // How many journeys we want
+//   var count = 300
 
-  // Save  ---------------------------------------------------
-    for(var i = 0; i< count; i++){
+//   // Save  ---------------------------------------------------
+//     for(var i = 0; i< count; i++){
 
-    departureCity = city[Math.floor(Math.random() * Math.floor(city.length))]
-    arrivalCity = city[Math.floor(Math.random() * Math.floor(city.length))]
+//     departureCity = city[Math.floor(Math.random() * Math.floor(city.length))]
+//     arrivalCity = city[Math.floor(Math.random() * Math.floor(city.length))]
 
-    if(departureCity != arrivalCity){
+//     if(departureCity != arrivalCity){
 
-      var newUser = new journeyModel ({
-        departure: departureCity , 
-        arrival: arrivalCity, 
-        date: date[Math.floor(Math.random() * Math.floor(date.length))],
-        departureTime:Math.floor(Math.random() * Math.floor(23)) + ":00",
-        price: Math.floor(Math.random() * Math.floor(125)) + 25,
-      });
+//       var newUser = new journeyModel ({
+//         departure: departureCity , 
+//         arrival: arrivalCity, 
+//         date: date[Math.floor(Math.random() * Math.floor(date.length))],
+//         departureTime:Math.floor(Math.random() * Math.floor(23)) + ":00",
+//         price: Math.floor(Math.random() * Math.floor(125)) + 25,
+//       });
        
-       await newUser.save();
+//        await newUser.save();
 
-    }
+//     }
 
-  }
-  res.render('index', { title: 'Express' });
-});
+//   }
+//   res.render('index', { title: 'Express' });
+// });
 
 
 // Cette route est juste une verification du Save.
