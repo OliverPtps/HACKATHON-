1b732d2c69b5de0644a3a8cd64635097c6c8bcad
Julien Noel <jncoweb@gmail.com>
2022-04-15 14:43:47 +0200

    check2

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var session = require("express-session");
 var models = require('./routes/index')
 
 var mongoose = require('./models/connection')
 
 var app = express();
+
+app.use(
+  session({
+   secret: 'a4f8071f-c873-4447-8ee2',
+   resave: false,
+   saveUninitialized: false,
+  })
+  );
 
 
 // view engine setup
